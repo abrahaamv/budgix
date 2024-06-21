75cd1823d540927d979e8ca60eb19574b7b1fa37
abrahaamv <velizabrahaam@gmail.com>
2024-06-21 11:10:18 -0600

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/1-Hero/Hero'
+import Main from './components/2-Main/Main'
+
+const App = () => {
+    return (
+        <>
+            <Hero />
+            <Main />
+        </>
+    )
+}
+
+export default App
---
+++ src/components/1-Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import './hero.css'
+
+function Hero() {
+
+  return (
+    <>
+      <div>
+        <a href="/" target="_blank">
+          <img src='./budgiX.svg' className="logo" alt="budgiX logo" />
+        </a>
+      </div>
+      <h1>The distro crafted to be Fast</h1>
+    </>
+  )
+}
+
+export default Hero
